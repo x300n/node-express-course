6e500accefab18e1fa4e081f7f319f4fe0622260
x300n <houseofxeon@gmail.com>
2020-08-12 01:06:29 +0200

    Added first GET route

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+{name: 'John Doe'},
+{name: 'Sara Jane'}
+]
+
+app.get('/', (req, res) => {
+
+  res.send("Great!!");
+
+})
+
+app.get('/users', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Successfully got users!',
+    users: mockUserData
+  })
+})
 
 app.listen(8000, () => {
 
   console.log("Server is running on local port 8000");
 
 })
-
