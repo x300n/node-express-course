6904ff0280eebf4225e1de6a766fab7ec6771d6e
x300n <houseofxeon@gmail.com>
2020-08-12 01:09:16 +0200

    Add dynamic GET route

--- server.js
+++ server.js
@@ -20,6 +20,19 @@ app.get('/users', (req, res) => {
   })
 })
 
+
+app.get('/users/:id', (req, res) => {
+  console.log(req.params.id);
+
+  res.json({
+    success: true,
+    message: 'Got one user',
+    user: req.params.id
+  })
+})
+
+
+
 app.listen(8000, () => {
 
   console.log("Server is running on local port 8000");
